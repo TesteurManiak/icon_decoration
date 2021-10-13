4a6bdc27e1981e1404d9824896661097476fe718
Guillaume Roux <rouxguillaume8@gmail.com>
2021-10-13 20:03:26 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,10 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Center(
